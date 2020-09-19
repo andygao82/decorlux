d85c0b6f99b6167c1cf2741ae1476914787226e4
andygao82 <freedomandy82@gmail.com>
2020-09-19 18:40:13 +1000

    update js

--- wp-content/themes/decorlux/js/custom.js
+++ wp-content/themes/decorlux/js/custom.js
@@ -22,15 +22,15 @@ jQuery(document).ready(function($){
     });
 
     $('#primary-menu > li.menu-item:nth-child(1)').mouseenter(function () {
-        $('.site-menu').css('background-image','url(http://localhost/decourlux/wp-content/themes/decorlux/images/curtain.jpg)');
+        $('.site-menu').css('background-image','url(http://decorluxnew.gtechsolution.com.au/wp-content/themes/decorlux/images/curtain.jpg)');
     });
 
     $('#primary-menu > li.menu-item:nth-child(2)').mouseenter(function () {
-        $('.site-menu').css('background-image','url(http://localhos/decourlux/t/wp-content/themes/decorlux/images/sofa.jpg)');
+        $('.site-menu').css('background-image','url(http://decorluxnew.gtechsolution.com.au/wp-content/themes/decorlux/images/sofa.jpg)');
     });
 
     $('#primary-menu > li.menu-item:nth-child(3)').mouseenter(function () {
-        $('.site-menu').css('background-image','url(http://localhost/decourlux//wp-content/themes/decorlux/images/decoration.jpg)');
+        $('.site-menu').css('background-image','url(http://decorluxnew.gtechsolution.com.au/wp-content/themes/decorlux/images/decoration.jpg)');
     });
 
     $('#primary-menu > li.menu-item').click(function () {
