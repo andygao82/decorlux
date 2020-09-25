6324f035f85a6b433bb2b15a3fd7f979f55ed513
andygao82 <freedomandy82@gmail.com>
2020-09-25 19:04:14 +1000

    updated ieasss

--- wp-content/themes/decorlux/single.php
+++ wp-content/themes/decorlux/single.php
@@ -76,7 +76,7 @@ $product_top_banner_heading = get_field('product_top_banner_content');
 		                            <?php endif?>
                                     <?php if(!empty(${"product_image_".$i}['caption'])):?>
                                         <li>
-                                            <span class="product-gallery-item">
+                                            <a class="product-gallery-item">
 			                                 <?php if(!empty($product_image_description)) :?>
                                                 <span class="product-gallery-description">
                                                     <span class="inner-wrapper">
@@ -90,10 +90,10 @@ $product_top_banner_heading = get_field('product_top_banner_content');
                                                 </span>
 			                                 <?php endif?>
                                                 <img src="<?= esc_url(${"product_image_".$i}['sizes']['large'])?>" alt="image">
-                                                <a href="<?= esc_url(${"product_image_".$i}['sizes']['large'])?>"  rel="product-gallery-img">
-                                                    <i class="fa fa-expand" aria-hidden="true"></i>
-                                                </a>
-                                            </span>
+                                            </a>
+                                            <a class="enlarge-icon" href="<?= esc_url(${"product_image_".$i}['sizes']['large'])?>"  rel="product-gallery-img">
+                                                <i class="fa fa-expand" aria-hidden="true"></i>
+                                            </a>
                                         </li>
                                     <?php else: ?>
                                         <li>
