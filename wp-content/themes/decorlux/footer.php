6a28122de2373f93c4874f31f278f7b6005bd1bb
andygao82 <freedomandy82@gmail.com>
2025-05-20 21:33:16 +1000

    wip

--- wp-content/themes/decorlux/footer.php
+++ wp-content/themes/decorlux/footer.php
@@ -17,12 +17,20 @@
 		<div class="container">
 			<div class="row">
 				<div class="col">
-					<div class="wrapper">
+					<div class="wrapper footer-top-wrapper">
 						<div class="footer-brief">
-							<h3>About Decorlux</h3>
-							<p></p>
+							<h3>
+								<?php 
+									echo get_field('footer_brief_heading', 'option');
+								?>
+							</h3>
+							<div class="footer-brief-content">
+								<?php 
+									echo get_field('footer_brief_content', 'option');
+								?>
+							</div>
 						</div>
-						<div class="footer-quick-link">
+						<div class="footer-quick-links">
 							<h3>Quick Links</h3>
 							<ul>
 								<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>about">About</a></li>
@@ -35,24 +43,34 @@
 							<h3>Follow Us</h3>
 							<ul class="social-media">
 								<li>
+									<a href="https://www.facebook.com/decorlux.curtain" target="_blank">
+										<i class="fa fa-facebook-square" aria-hidden="true"></i>
+									</a>
+								</li>
+								<li>
 									<a href="https://www.instagram.com/decorlux_homestaging/" target="_blank">
 										<i class="fa fa-instagram" aria-hidden="true"></i>
 									</a>
 								</li>
-								<li>
-									<a href="https://www.facebook.com/decorlux.curtain" target="_blank">
-										<i class="fa fa-facebook" aria-hidden="true"></i>
-									</a>
-								</li>
 							</ul>
 						</div>
-						<div class="address">
-							<p>185 Cheltenham Rd, Keysborough VIC 3173</p>
+						<div class="footer-address">
+							<h3>Showroom Address</h3>
+							<div class="address-content">
+								<i class="fa fa-map-marker" aria-hidden="true"></i>
+
+								<?php 
+									echo get_field('showroom_address', 'option');
+								?>
+							</div>
+							
 						</div>
 					</div>
 				</div>
+			</div>
+			<div class="row">
 				<div class="col">
-					<div class="wrapper">
+					<div class="wrapper footer-bottom-wrapper">
 						<div class="copyright">Copyright &copy; <?= date('Y') ?> DecorLUX. All Rights Reserved</div>
 						<div class="footer-links">
 							<ul>
