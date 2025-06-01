2a5c4e796240415648de31d9dbe9f11181c2a194
andygao82 <andy@zenu.com.au>
2025-06-01 10:10:07 +1000

    wip

---
+++ wp-content/themes/decorlux/single-backupy.php
@@ -0,0 +1,119 @@
+<?php
+
+get_header();
+
+$product_description = get_field('product_description');
+$categories = get_the_category();
+$product_top_banner_image = get_field('product_top_banner_image');
+$product_top_banner_heading = get_field('product_top_banner_content');
+?>
+<?php if($product_top_banner_image){?>
+    <section class="page-banner product-banner cat-<?php echo $categories[0]->cat_ID ?>" style="background-image: url('<?= esc_url($product_top_banner_image)?>')">
+<?php }else{?>
+    <section class="page-banner product-banner cat-<?php echo $categories[0]->cat_ID ?>">
+<?php }?>
+        <div class="page-banner-wrapper">
+            <?php if($product_top_banner_heading){?>
+            <h1 class="page-title"><?php echo $product_top_banner_heading ?></h1>
+            <?php } ?>
+            <img src="<?php echo get_template_directory_uri('/'); ?>/images/mouse.svg" alt="mouse" id="mouse">
+        </div>
+    </section>
+    <section class="product-details">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="wrapper">
+                        <div class="product-detail-left">
+                            <div class="product-cat"><?php echo $categories[0]->name; ?></div>
+                            <div class="product-range"><?php the_title() ?></div>
+                            <?php $img_path = get_template_directory_uri('/'); ?>
+                            <div class="product-more">
+                                <a class="product-detail-button" href="javascript:;" onclick="goBack()"><span>Back</span></a>
+                            </div>
+                            <?php if(get_field('pdf_upload')):?>
+                            <div class="product-details-file">
+                                <a class="product-detail-button" href="<?= get_field('pdf_upload')?>" target="_blank"><span>Details</span></a>
+                            </div>
+                            <?php endif ?>
+                        </div>
+	                    <?php if(get_field('main_image')): ?>
+                        <div class="product-detail-right">
+                            <div class="product-detail-right-wrapper">
+                                <img src="<?= get_field('main_image') ?>" alt="<?php the_title() ?>" title="<?php the_title() ?>">
+                            </div>
+                        </div>
+	                    <?php endif ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="gallery-section">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="wrapper">
+                        <?php if(!empty(get_field('gallery_title'))){ ?>
+                        <h2>
+                            <span><?= get_field('gallery_title') ? get_field('gallery_title') : ''?></span>
+                        </h2>
+                        <?php }?>
+                        <ul>
+                            <?php for($i = 1; $i <=30; $i++):?>
+                                <?php if(get_field('product_image_'.$i.'')) {?>
+                                    <?php ${"product_image_".$i} = get_field('product_image_'.$i.'') ?>
+		                            <?php $product_image_description = '' ?>
+		                            <?php if(!empty(${"product_image_".$i}['description'])) :?>
+		                                <?php $product_image_description = ${"product_image_".$i}['description'] ?>
+		                            <?php endif?>
+                                    <?php if(!empty(${"product_image_".$i}['caption'])):?>
+                                        <li>
+                                            <a class="product-gallery-item" href="javascript:;">
+			                                 <?php if(!empty($product_image_description)) :?>
+                                                <span class="product-gallery-description">
+                                                    <span class="inner-wrapper">
+                                                        <?php if(!empty(${"product_image_".$i}['title'])) :?>
+                                                            <h4><?= ${"product_image_".$i}['title'] ?></h4>
+                                                        <?php endif ?>
+                                                        <?php if(!empty($product_image_description)) :?>
+                                                            <span><?php echo nl2br($product_image_description) ?></span>
+                                                        <?php endif?>
+                                                    </span>
+                                                </span>
+			                                 <?php endif?>
+                                                <img src="<?= esc_url(${"product_image_".$i}['sizes']['large'])?>" alt="image">
+                                            </a>
+                                            <a class="enlarge-icon" href="<?= esc_url(${"product_image_".$i}['sizes']['large'])?>"  rel="product-gallery-img">
+                                                <i class="fa fa-expand" aria-hidden="true"></i>
+                                            </a>
+                                        </li>
+                                    <?php else: ?>
+                                        <li>
+                                            <a class="product-gallery-item"  href="javascript:;" rel="product-gallery-img" >
+			                                <?php if(!empty($product_image_description)) :?>
+                                                <span class="product-gallery-description">
+                                                    <span class="inner-wrapper">
+                                                        <?php if(!empty(${"product_image_".$i}['title'])) :?>
+                                                            <h4><?= ${"product_image_".$i}['title'] ?></h4>
+                                                        <?php endif ?>
+	                                                    <?php if(!empty($product_image_description)) :?>
+                                                            <span><?php echo nl2br($product_image_description) ?></span>
+	                                                    <?php endif?>
+                                                    </span>
+                                                </span>
+                                            <?php endif?>
+                                                <img src="<?= ${'product_image_'.$i}['sizes']['large']?>" alt="image">
+                                            </a>
+                                        </li>
+                                    <?php endif ?>
+                                <?php }?>
+	                        <?php endfor?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php
+get_footer();
--- wp-content/themes/decorlux/single.php
+++ wp-content/themes/decorlux/single.php
@@ -6,121 +6,135 @@
  *
  * @package Decorlux
  */
-
 get_header();
-
-$product_description = get_field('product_description');
-$categories = get_the_category();
-$product_top_banner_image = get_field('product_top_banner_image');
-$product_top_banner_heading = get_field('product_top_banner_content');
+	
+	if (have_rows('product_details')):
+		while (have_rows('product_details')) : the_row();
+		$product_description = get_field('product_description');
+		$categories = get_the_category();
+		$product_top_banner_heading = get_field('product_top_banner_content');
 ?>
-<?php if($product_top_banner_image){?>
-    <section class="page-banner product-banner cat-<?php echo $categories[0]->cat_ID ?>" style="background-image: url('<?= esc_url($product_top_banner_image)?>')">
-<?php }else{?>
-    <section class="page-banner product-banner cat-<?php echo $categories[0]->cat_ID ?>">
-<?php }?>
-        <div class="page-banner-wrapper">
-            <?php if($product_top_banner_heading){?>
-            <h1 class="page-title"><?php echo $product_top_banner_heading ?></h1>
-            <?php } ?>
-            <img src="<?php echo get_template_directory_uri('/'); ?>/images/mouse.svg" alt="mouse" id="mouse">
-        </div>
-    </section>
-    <section class="product-details">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="wrapper">
-                        <div class="product-detail-left">
-                            <div class="product-cat"><?php echo $categories[0]->name; ?></div>
-                            <div class="product-range"><?php the_title() ?></div>
-                            <?php $img_path = get_template_directory_uri('/'); ?>
-                            <div class="product-more">
-                                <a class="product-detail-button" href="javascript:;" onclick="goBack()"><span>Back</span></a>
-                            </div>
-                            <?php if(get_field('pdf_upload')):?>
-                            <div class="product-details-file">
-                                <a class="product-detail-button" href="<?= get_field('pdf_upload')?>" target="_blank"><span>Details</span></a>
-                            </div>
-                            <?php endif ?>
-                        </div>
-	                    <?php if(get_field('main_image')): ?>
-                        <div class="product-detail-right">
-                            <div class="product-detail-right-wrapper">
-                                <img src="<?= get_field('main_image') ?>" alt="<?php the_title() ?>" title="<?php the_title() ?>">
-                            </div>
-                        </div>
-	                    <?php endif ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="gallery-section">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="wrapper">
-                        <?php if(!empty(get_field('gallery_title'))){ ?>
-                        <h2>
-                            <span><?= get_field('gallery_title') ? get_field('gallery_title') : ''?></span>
-                        </h2>
-                        <?php }?>
-                        <ul>
-                            <?php for($i = 1; $i <=30; $i++):?>
-                                <?php if(get_field('product_image_'.$i.'')) {?>
-                                    <?php ${"product_image_".$i} = get_field('product_image_'.$i.'') ?>
-		                            <?php $product_image_description = '' ?>
-		                            <?php if(!empty(${"product_image_".$i}['description'])) :?>
-		                                <?php $product_image_description = ${"product_image_".$i}['description'] ?>
-		                            <?php endif?>
-                                    <?php if(!empty(${"product_image_".$i}['caption'])):?>
-                                        <li>
-                                            <a class="product-gallery-item" href="javascript:;">
-			                                 <?php if(!empty($product_image_description)) :?>
-                                                <span class="product-gallery-description">
-                                                    <span class="inner-wrapper">
-                                                        <?php if(!empty(${"product_image_".$i}['title'])) :?>
-                                                            <h4><?= ${"product_image_".$i}['title'] ?></h4>
-                                                        <?php endif ?>
-                                                        <?php if(!empty($product_image_description)) :?>
-                                                            <span><?php echo nl2br($product_image_description) ?></span>
-                                                        <?php endif?>
-                                                    </span>
-                                                </span>
-			                                 <?php endif?>
-                                                <img src="<?= esc_url(${"product_image_".$i}['sizes']['large'])?>" alt="image">
-                                            </a>
-                                            <a class="enlarge-icon" href="<?= esc_url(${"product_image_".$i}['sizes']['large'])?>"  rel="product-gallery-img">
-                                                <i class="fa fa-expand" aria-hidden="true"></i>
-                                            </a>
-                                        </li>
-                                    <?php else: ?>
-                                        <li>
-                                            <a class="product-gallery-item"  href="javascript:;" rel="product-gallery-img" >
-			                                <?php if(!empty($product_image_description)) :?>
-                                                <span class="product-gallery-description">
-                                                    <span class="inner-wrapper">
-                                                        <?php if(!empty(${"product_image_".$i}['title'])) :?>
-                                                            <h4><?= ${"product_image_".$i}['title'] ?></h4>
-                                                        <?php endif ?>
-	                                                    <?php if(!empty($product_image_description)) :?>
-                                                            <span><?php echo nl2br($product_image_description) ?></span>
-	                                                    <?php endif?>
-                                                    </span>
-                                                </span>
-                                            <?php endif?>
-                                                <img src="<?= ${'product_image_'.$i}['sizes']['large']?>" alt="image">
-                                            </a>
-                                        </li>
-                                    <?php endif ?>
-                                <?php }?>
-	                        <?php endfor?>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+	<?php if (get_row_layout() == 'main_content'): ?>
+	<div class="banner cat-<?php echo $categories[0]->cat_ID ?>" style="background-image: url('<?= get_sub_field('image')?>')">
+
+	</div>
+	<div class="section page-category-top">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="inner-wrapper">
+						<a class="back-button" href="javascript:;">
+							<span class="material-symbols-outlined icon">arrow_back_ios</span> 
+							<span>Back</span>
+						</a>
+						<div class="page-category-description">
+							<h1 class="post-title"><?php echo get_sub_field('product_top_banner_content') ?></h1>
+							<div class="archive-description">
+								<?php echo get_sub_field('product_description') ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php endif; ?>
+<?php endwhile; endif; ?>
+	<section class="product-details">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="wrapper">
+						<div class="product-detail-left">
+							<div class="product-cat"><?php echo $categories[0]->name; ?></div>
+							<div class="product-range"><?php the_title() ?></div>
+							<?php $img_path = get_template_directory_uri('/'); ?>
+							<div class="product-more">
+								<a class="product-detail-button" href="javascript:;" onclick="goBack()"><span>Back</span></a>
+							</div>
+							<?php if(get_field('pdf_upload')):?>
+							<div class="product-details-file">
+								<a class="product-detail-button" href="<?= get_field('pdf_upload')?>" target="_blank"><span>Details</span></a>
+							</div>
+							<?php endif ?>
+						</div>
+						<?php if(get_field('main_image')): ?>
+						<div class="product-detail-right">
+							<div class="product-detail-right-wrapper">
+								<img src="<?= get_field('main_image') ?>" alt="<?php the_title() ?>" title="<?php the_title() ?>">
+							</div>
+						</div>
+						<?php endif ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="gallery-section">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="wrapper">
+						<?php if(!empty(get_field('gallery_title'))){ ?>
+						<h2>
+							<span><?= get_field('gallery_title') ? get_field('gallery_title') : ''?></span>
+						</h2>
+						<?php }?>
+						<ul>
+							<?php for($i = 1; $i <=30; $i++):?>
+								<?php if(get_field('product_image_'.$i.'')) {?>
+									<?php ${"product_image_".$i} = get_field('product_image_'.$i.'') ?>
+									<?php $product_image_description = '' ?>
+									<?php if(!empty(${"product_image_".$i}['description'])) :?>
+										<?php $product_image_description = ${"product_image_".$i}['description'] ?>
+									<?php endif?>
+									<?php if(!empty(${"product_image_".$i}['caption'])):?>
+										<li>
+											<a class="product-gallery-item" href="javascript:;">
+											 <?php if(!empty($product_image_description)) :?>
+												<span class="product-gallery-description">
+													<span class="inner-wrapper">
+														<?php if(!empty(${"product_image_".$i}['title'])) :?>
+															<h4><?= ${"product_image_".$i}['title'] ?></h4>
+														<?php endif ?>
+														<?php if(!empty($product_image_description)) :?>
+															<span><?php echo nl2br($product_image_description) ?></span>
+														<?php endif?>
+													</span>
+												</span>
+											 <?php endif?>
+												<img src="<?= esc_url(${"product_image_".$i}['sizes']['large'])?>" alt="image">
+											</a>
+											<a class="enlarge-icon" href="<?= esc_url(${"product_image_".$i}['sizes']['large'])?>"  rel="product-gallery-img">
+												<i class="fa fa-expand" aria-hidden="true"></i>
+											</a>
+										</li>
+									<?php else: ?>
+										<li>
+											<a class="product-gallery-item"  href="javascript:;" rel="product-gallery-img" >
+											<?php if(!empty($product_image_description)) :?>
+												<span class="product-gallery-description">
+													<span class="inner-wrapper">
+														<?php if(!empty(${"product_image_".$i}['title'])) :?>
+															<h4><?= ${"product_image_".$i}['title'] ?></h4>
+														<?php endif ?>
+														<?php if(!empty($product_image_description)) :?>
+															<span><?php echo nl2br($product_image_description) ?></span>
+														<?php endif?>
+													</span>
+												</span>
+											<?php endif?>
+												<img src="<?= ${'product_image_'.$i}['sizes']['large']?>" alt="image">
+											</a>
+										</li>
+									<?php endif ?>
+								<?php }?>
+							<?php endfor?>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 <?php
 get_footer();
